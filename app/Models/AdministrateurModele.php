da0970f6eb82960acc48d2cb62ca9716ef31bae4
Romain <romain.gascoin@gmail.com>
2024-01-25 11:52:26 +0100

    ajout du script de création et de 2 modèles

---
+++ app/Models/AdministrateurModele.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+class AdministrateurModele extends Model
+{
+    // nom de la table gérée par ce modèle
+    protected $table = 'administrateur';
+
+    // clé primaire de la table
+    protected $primaryKey = 'idAdministrateur';
+
+    // constructeur
+    public function __construct()
+    {
+        // super()
+        parent::__construct();
+
+        // création de la table si elle n'existe pas déjà
+        $this->forge = \Config\Database::forge();
+        $this->db = \Config\Database::connect();
+
+        if (!$this->db->tableExists($this->table))
+        {
+            $fields = [
+                'idAdministrateur' => [
+                    'type' => 'INT',
+                    'constraint' => 11,
+                    'unsigned' => true,
+                    'auto_increment' => true,
+                ],
+            ];
+
+            $this->forge->addField($fields);
+            $this->forge->addKey('idAdministrateur', TRUE); // clé primaire
+            $this->forge->addForeignKey('idAdministrateur','Enseignant','idEnseignant'); // clé étrangère
+            $this->forge->createTable($this->table, TRUE);
+        }
+
+        // charger les données
+        $this->db = \Config\Database::connect();
+    }
+
+}
