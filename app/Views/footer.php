e17beb97ee1e97d88a032579ef3d788a3b04aeb4
florian-bizet <sunburnstar84@gmail.com>
2024-01-30 17:32:51 +0100

    Page d'accueil

---
+++ app/Views/footer.php
@@ -0,0 +1,7 @@
+<div class="container">
+        <footer class="py-3 my-4">
+          <p class="text-center text-body-secondary border-top py-2">SGRDS 2024</p>
+        </footer>
+      </div>
+</body>
+</html>
