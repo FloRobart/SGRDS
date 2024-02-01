33330bc7f0ed677142d48be677f91fac72c1a6c0
Floris <FlorisRobart.pro@gmail.com>
2024-02-01 17:01:30 +0100

    correction de bug

--- app/Views/updatePasswordVue.php
+++ app/Views/updatePasswordVue.php
@@ -1,15 +1,11 @@
 <?php echo view('header'); ?>
 <div class="row">
     <div class="col-md-8 offset-md-2 text-center my-5">
-    <?php
-        echo $succes ? '<span class="text-center">Mot de passe réinitialisé avec succès</span><br />' : '<span class="text-center">Erreur lors de la réinitialisation du mot de passe</span><br />';
-    ?>
-    <!-- <span>Mot de passe réinitialisé avec succès.</span><br /><input type="button" value="Retour à la page de connexion" onclick="window.location.href=\">
-    <span>Erreur lors de la réinitialisation du mot de passe.</span><br /><input type="button" value="Retour à la page de connexion" onclick="window.location.href=\'/connexion\'">
-
-<div class="row">
-    <div class="col-md-8 offset-md-2 text-center my-5">
-        <span class="text-center">Mot de passe réinitialisé avec succès</span><br /> -->    
+        <h1 class="text-center">
+            <?php
+                echo $success ? 'Mot de passe réinitialisé avec succès' : 'Erreur lors de la réinitialisation du mot de passe';
+            ?>
+        </h1><br />
         <a href="/connexion" class="btn btn-primary my-5 py-1">Connexion</a>
     </div>
 </div>
